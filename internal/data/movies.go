9dfa2b6c556ac2c1764f8213f077ec93db612083
k1nho <kinhong99@gmail.com>
2023-06-22 15:09:07 -0400

    movies model

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -1,9 +1,12 @@
 package data
 
 import (
+	"database/sql"
+	"errors"
 	"time"
 
 	"github.com/k1nho/letsgo/internal/validator"
+	"github.com/lib/pq"
 )
 
 type Movie struct {
@@ -39,4 +42,54 @@ func ValidateMovie(v *validator.Validator, m *Movie) {
 	v.Check(len(m.Genres) <= 5, "genres", "must not exceeed 5 genres")
 	v.Check(validator.Unique(m.Genres), "genres", "must not contain duplicates")
 
+}
+
+type MovieModel struct {
+	DB *sql.DB
+}
+
+func (m MovieModel) Get(id int64) (*Movie, error) {
+
+	if id < 1 {
+		return nil, ErrRecordNotFound
+	}
+
+	query := `
+        SELECT id, created_at, title, year, runtime, genres, version
+        FROM movies
+        WHERE id=$1
+    `
+
+	var movie Movie
+
+	err := m.DB.QueryRow(query, id).Scan(&movie.ID, &movie.CreatedAt, &movie.Title, &movie.Year, &movie.Runtime, pq.Array(&movie.Genres), &movie.Version)
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return nil, ErrRecordNotFound
+		default:
+			return nil, err
+		}
+	}
+	return &movie, nil
+}
+
+func (m MovieModel) Insert(movie *Movie) error {
+	query := `
+        INSERT INTO movies(title, year, runtime, genres)
+        VALUES($1, $2, $3, $4)
+        RETURNING id, created_at, version
+    `
+
+	args := []interface{}{movie.Title, movie.Year, movie.Runtime, pq.Array(movie.Genres)}
+
+	return m.DB.QueryRow(query, args...).Scan(&movie.ID, &movie.CreatedAt, &movie.Version)
+}
+
+func (m MovieModel) Update(id int64) error {
+	return nil
+}
+
+func (m MovieModel) Delete(id int64) error {
+	return nil
 }
