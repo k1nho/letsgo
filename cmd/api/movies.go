3358c02235fc6f8f62095fba426de50190ac119e
k1nho <kinhong99@gmail.com>
2023-06-22 15:09:38 -0400

    using queries on the handlers

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
-	"time"
 
 	"github.com/k1nho/letsgo/internal/data"
 	"github.com/k1nho/letsgo/internal/validator"
@@ -37,7 +37,20 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 		return
 	}
 
-	fmt.Fprintf(w, "%+v\n", input)
+	err = app.models.Movies.Insert(m)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	headers := make(http.Header)
+	headers.Set("Location", fmt.Sprintf("/v1/movies/%d", m.ID))
+
+	err = app.WriteJson(w, http.StatusCreated, envelope{"movie": m}, headers)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+
 }
 
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -47,14 +60,15 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
-	movie := data.Movie{
-		ID:        id,
-		CreatedAt: time.Now(),
-		Title:     "The Matrix",
-		Year:      1999,
-		Runtime:   201,
-		Genres:    []string{"psychological", "Avant-Garde", "Action"},
-		Version:   1,
+	movie, err := app.models.Movies.Get(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
 	}
 
 	err = app.WriteJson(w, http.StatusOK, envelope{"movie": movie}, nil)
