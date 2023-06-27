5ae99e69b03dcc78f67b2aee03ba75ca2426edd3
k1nho <kinhong99@gmail.com>
2023-06-27 13:18:09 -0400

    rate limiters

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"net/http"
+	"sync"
+	"time"
 
 	"golang.org/x/time/rate"
 )
@@ -20,6 +23,8 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 	})
 }
 
+/*
+-- GLOBAL RATE LIMITER
 func (app *application) rateLimit(next http.Handler) http.Handler {
 
 	limiter := rate.NewLimiter(2, 4)
@@ -29,6 +34,64 @@ func (app *application) rateLimit(next http.Handler) http.Handler {
 			app.rateLimitExceededResponse(w, r)
 			return
 		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+*/
+
+// IP-BASED Rate limiter
+func (app *application) rateLimit(next http.Handler) http.Handler {
+
+	type client struct {
+		limiter  *rate.Limiter
+		lastSeen time.Time
+	}
+
+	var (
+		mu      sync.Mutex
+		clients = make(map[string]*client)
+	)
+
+	// This goroutine spins up every minute and cleans up clients that have not used the API within 3 minutes
+	// This allows our clients map to ever grow infinitely
+	go func() {
+		for {
+			time.Sleep(time.Minute)
+			mu.Lock()
+
+			for ip, client := range clients {
+				if time.Since(client.lastSeen) > 3*time.Minute {
+					delete(clients, ip)
+				}
+			}
+			mu.Unlock()
+		}
+	}()
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ip, _, err := net.SplitHostPort(r.RemoteAddr)
+		if err != nil {
+			app.serverErrorResponse(w, r, err)
+			return
+		}
+
+		mu.Lock()
+
+		if _, found := clients[ip]; !found {
+			clients[ip] = &client{limiter: rate.NewLimiter(2, 4)}
+		}
+
+		clients[ip].lastSeen = time.Now()
+
+		if !clients[ip].limiter.Allow() {
+			mu.Unlock()
+			app.rateLimitExceededResponse(w, r)
+			return
+		}
+
+		mu.Unlock()
 
 		next.ServeHTTP(w, r)
 	})
